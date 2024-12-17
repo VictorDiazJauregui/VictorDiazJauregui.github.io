a8da3c80bfc86f1424b23f4b5ca6ccf6eecb73ff
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-12-16 22:29:06 -0300

    Configuraciónes para el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://victordiazjauregui.github.io/',
+  site: 'https://victordiazjauregui.github.io',
+  base: '/',
+  build: {
+    assets: '_assets'
+  },
   devToolbar: {
     enabled: false,
   }
